684b0e2f72386df8b12031984020641187eb64af
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-05-30 16:58:30 +0200

    Disabled possibility to go back from login activity to main (logo) activity. Add CountDownTimer to main activity if no user is logged in. Add logo image placeholder.

--- app/src/main/java/com/ravensu/gaitpodzialy/MainActivity.java
+++ app/src/main/java/com/ravensu/gaitpodzialy/MainActivity.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -22,23 +25,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //debug
-//        SharedPreferences appLogins = getSharedPreferences("app_logins", MODE_PRIVATE);
-//        SharedPreferences.Editor appLoginsEditor = appLogins.edit();
-//        appLoginsEditor.remove(TestingValues.username);
-//        appLoginsEditor.apply();
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        SharedPreferences appLogins = getSharedPreferences("app_logins", MODE_PRIVATE);
+        SharedPreferences.Editor appLoginsEditor = appLogins.edit();
+        appLoginsEditor.remove(TestingValues.username);
+        appLoginsEditor.apply();
 //        debug end
         Log.d("MainActivity", "onCreate: "  + AppLogins.GetAllCredidentials(this).size());
         Log.d("MainActivity", "onCreate: "  + AppMainLogin.GetMainLoginUserName(this));
         if (!AppLogins.ExistsAny(this)) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivityForResult(intent, Activity.RESULT_OK);
+            new CountDownTimer(2000, 1000){
+                @Override
+                public void onTick(long millisUntilFinished) {}
+
+                @Override
+                public void onFinish() {
+                    loginActivity();
+                }
+            }.start();
         }
         else Toast.makeText(getApplicationContext(), "Welcome " + AppMainLogin.GetMainLoginUserName(this), Toast.LENGTH_LONG).show();
         //start new intent with tables
+    }
+
+    private void loginActivity(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivityForResult(intent, Activity.RESULT_OK);
     }
 }
