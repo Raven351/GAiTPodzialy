785ad0e7aa03969080196043ba2ab9dc4ed1b27c
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-06-01 21:34:14 +0200

    Add fragment layout for today and tomorrow assignments

---
+++ app/src/main/java/com/ravensu/gaitpodzialy/activities/ui/assignmentslist/AssignmentsListCurrentsViewModelFragment.java
@@ -0,0 +1,7 @@
+package com.ravensu.gaitpodzialy.activities.ui.assignmentslist;
+
+import androidx.lifecycle.ViewModel;
+
+public class AssignmentsListCurrentsViewModelFragment extends ViewModel {
+    // TODO: Implement the ViewModel
+}
