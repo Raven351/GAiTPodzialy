1635ecef6f3629c62c4a0e7ee3297c8b6edcbc75
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-10-05 18:32:29 +0200

    Update about Activity

--- app/src/main/java/com/ravensu/gaitpodzialy/activities/ui/AboutActivity.java
+++ app/src/main/java/com/ravensu/gaitpodzialy/activities/ui/AboutActivity.java
@@ -3,6 +3,8 @@ package com.ravensu.gaitpodzialy.activities.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.ravensu.gaitpodzialy.R;
 
@@ -12,5 +14,15 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setupHyperlinks();
+    }
+
+    private void setupHyperlinks(){
+        TextView textViewMadeBy = findViewById(R.id.textview_made_by);
+        TextView textViewAbout = findViewById(R.id.textview_about);
+        TextView textViewAboutMoreInfo = findViewById(R.id.textview_about_more_info);
+        textViewMadeBy.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewAbout.setMovementMethod(LinkMovementMethod.getInstance());
+        textViewAboutMoreInfo.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
