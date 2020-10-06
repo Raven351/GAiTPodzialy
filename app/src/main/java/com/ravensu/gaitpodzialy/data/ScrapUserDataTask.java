9efe2a8e5fb62ed9398545b9f3416f273f8d3ee8
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-10-06 18:12:04 +0200

    Add ScrapUserDataTask

---
+++ app/src/main/java/com/ravensu/gaitpodzialy/data/ScrapUserDataTask.java
@@ -0,0 +1,33 @@
+package com.ravensu.gaitpodzialy.data;
+
+import com.ravensu.gaitpodzialy.webscrapper.GAiTWebScrapper;
+import com.ravensu.gaitpodzialy.webscrapper.models.Assignment;
+import com.ravensu.gaitpodzialy.webscrapper.models.User;
+
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+public class ScrapUserDataTask implements Callable<User> {
+    private String UserId;
+    private String UserPassword;
+
+    public ScrapUserDataTask(String userId, String userPassword){
+        this.UserId = userId;
+        this.UserPassword = userPassword;
+    }
+
+    @Override
+    public User call() {
+        User user = new User(UserId, UserPassword, new ArrayList<Assignment>(), new ArrayList<com.ravensu.gaitpodzialy.webscrapper.models.Document>(), false);
+        GAiTWebScrapper gAiTWebScrapper = new GAiTWebScrapper(UserId, UserPassword);
+        Document gAiTWebsite = gAiTWebScrapper.GetGAiTWebsite();
+        if (gAiTWebsite != null){
+            ArrayList<Assignment> assignments = gAiTWebScrapper.ScrapAssignmentsTable(gAiTWebsite);
+            ArrayList<com.ravensu.gaitpodzialy.webscrapper.models.Document> documents = gAiTWebScrapper.ScrapDocumentsTable(gAiTWebsite);
+            user = new User(UserId, UserPassword, assignments, documents, true);
+        }
+        return user;
+    }
+}
