9874e71b008743497e293bc132141802de1d60fa
Bartosz Baum <35452429+Raven351@users.noreply.github.com>
2020-10-24 10:39:26 +0200

    Update AboutActivity and strings.xml langauge resource files

--- app/src/main/java/com/ravensu/gaitpodzialy/activities/ui/AboutActivity.java
+++ app/src/main/java/com/ravensu/gaitpodzialy/activities/ui/AboutActivity.java
@@ -2,6 +2,7 @@ package com.ravensu.gaitpodzialy.activities.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
@@ -15,14 +16,22 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         setupHyperlinks();
+        setupVersionName();
+    }
+
+    private void setupVersionName() {
+        TextView textViewVersion = findViewById(R.id.version);
+        try {
+            textViewVersion.append(this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private void setupHyperlinks(){
         TextView textViewMadeBy = findViewById(R.id.textview_made_by);
         TextView textViewAbout = findViewById(R.id.textview_about);
-        TextView textViewAboutMoreInfo = findViewById(R.id.textview_about_more_info);
         textViewMadeBy.setMovementMethod(LinkMovementMethod.getInstance());
         textViewAbout.setMovementMethod(LinkMovementMethod.getInstance());
-        textViewAboutMoreInfo.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
